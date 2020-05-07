e6637bb9435864f17e3912a5cf50894afd8036fd
Largenty <Largenty@gmail.com>
2020-05-08 01:44:41 +0200

    header + sidebar + axios install ok

---
+++ twitchclonereact/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Header from './components/Header/Header'
+import SideBar from './components/SideBar/SideBar'
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <SideBar />
+    </div>
+  );
+}
+
+export default App;
---
+++ twitchclonereact/src/api.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import axios from 'axios';
+
+let api = axios.create({
+    headers: {
+        'Client-ID' : '66iopfoz8dg1k3ost4s1f2annij3ee'
+    }
+})
+
+export default api;
