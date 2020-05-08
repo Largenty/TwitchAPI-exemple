07eac1e120043386fb4941058d2c607f6cec84dd
Largenty <Largenty@gmail.com>
2020-05-08 14:38:26 +0200

    call axios api for sidebar

--- twitchclonereact/src/components/SideBar/SideBar.js
+++ twitchclonereact/src/components/SideBar/SideBar.js
@@ -11,7 +11,7 @@ const SideBar = () => {
       
       const result = await api.get('https://api.twitch.tv/helix/streams')
       let dataArray = result.data.data;
-      console.log(dataArray);
+      // console.log(dataArray);
 
       let gameIDS = dataArray.map(stream => {
         return stream.game_id;
@@ -21,16 +21,67 @@ const SideBar = () => {
         return stream.user_id;
       })
 
-      console.log(gameIDS, userIDS);
-      
+      // console.log(gameIDS, userIDS);
 
+      // Create URLS perso
 
-      
-    
+      let baseUrlGames = "https://api.twitch.tv/helix/games?";
+      let baseUrlUsers = "https://api.twitch.tv/helix/users?";
+
+      let queryParamsGame = "";
+      let queryParamsUsers = "";
+
+      gameIDS.map( id => {
+        return (queryParamsGame = queryParamsGame + `id=${id}&`)
+      })
+
+      userIDS.map( id => {
+        return (queryParamsUsers = queryParamsUsers + `id=${id}&`)
+      })
+
+      //url final
+
+      let urlFinalGames = baseUrlGames + queryParamsGame;
+      let urlFinalUsers = baseUrlUsers + queryParamsUsers;
+
+      // console.log(urlFinalGames, urlFinalUsers)
+
+      // call
+      let gamesNames = await api.get(urlFinalGames);
+      let getUsers = await api.get(urlFinalUsers);
+
+      let gamesNameArray = gamesNames.data.data;
+      let arrayUsers = getUsers.data.data;
+      // console.log(gamesNameArray, arrayUsers);
+
+      // create final []
+      let finalArray = dataArray.map( stream => {
+        stream.gameName = "";
+        stream.truePic = "";
+        stream.login= "";
+
+        gamesNameArray.forEach(name => {
+          arrayUsers.forEach( user => {
+            if(stream.user_id === user.id && stream.game_id === name.id) {
+
+              stream.truePic = user.profile_image_url;
+              stream.gameName = name.name;
+              stream.login = user.login;
+
+            }
+          })
+        })
+        return stream;
+      })
+
+      setTopStreams(finalArray.slice(0, 8));    
     }
 
     fetchData();
   }, [])
+
+  console.log(topStreams);
+  
 
   return (
     <div className="sidebar">
