f60029d2cd9f0478ca763fa52936d99e72a8babc
Largenty <Largenty@gmail.com>
2020-05-08 15:39:44 +0200

    install router and create top streams

--- twitchclonereact/src/App.js
+++ twitchclonereact/src/App.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header/Header'
-import SideBar from './components/SideBar/SideBar'
-import Games from './components/Games/Games'
-
+import Header from './components/Header/Header';
+import SideBar from './components/SideBar/SideBar';
+import Games from './components/Games/Games';
+import TopStreams from './components/TopStreams/TopStreams';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <SideBar />
-      <Games />
-    </div>
+    <Router >
+      <div className="App">
+        <Header />
+        <SideBar />
+        <Switch >
+          <Route exact path="/" component={Games} />
+          <Route exact path="/top-streams" component={TopStreams} />
+
+        </Switch>
+        
+      </div>
+    </Router>
   );
 }
 
---
+++ twitchclonereact/src/components/TopStreams/TopStreams.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TopStreams = () => {
+
+
+    return (
+        <h1>je suis TopStreams</h1>
+    )
+}
+
+export default TopStreams;
