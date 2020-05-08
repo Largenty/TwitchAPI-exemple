f29d1e7e7acd7303c127f8161883ab28b95d2553
Largenty <Largenty@gmail.com>
2020-05-08 13:45:36 +0200

    create card top game with map

--- twitchclonereact/src/components/Games/Games.js
+++ twitchclonereact/src/components/Games/Games.js
@@ -11,22 +11,47 @@ const Games = () => {
       const result = await api.get('https://api.twitch.tv/helix/games/top')
       console.log(result);
 
+      let dataArray = result.data.data;
+      let finalArray = dataArray.map(game => {
+        let newUrl = game.box_art_url
+        .replace("{width}", "250")
+        .replace("{height}", "300")
+      game.box_art_url = newUrl;
+      return game;
+      });
+
+      setGames(finalArray)
     }
 
     fetchData();
   }, [])
+
+  console.log(games)
   
   return(
     <div>
       <h1 className="titreGames">Jeux les plus populaires</h1>
+
       <div className="flexAccueil">
+        {games.map((game,index) => (
+
+          <div key={index} className="carteGames">
+            <img src={game.box_art_url} alt="jeu profile image" className="imgCarte"/>
+
+            <div className="cardBodyGames">
+              <h5 className="titreCartesGames">{game.name}</h5>
+
+              <div className="btnCarte">
+                Regarder {game.name}
+              </div>              
+            </div>
+
+          </div>
+        ))}
 
       </div>
 
-
-
     </div>
-
       
   )
 }
